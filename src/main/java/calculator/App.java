646ee8c0ffbaf010a078ed5d6ad6b85b66c5a023
안동환 <ddh1713flrwls1713@gmail.com>
2024-07-29 02:32:12 +0900

    step1ing

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,9 +1,22 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
+        int num1 = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
+        int num2 = sc.nextInt();
+
+        // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장한다. (charAt(0))
+
+
     }
 }
