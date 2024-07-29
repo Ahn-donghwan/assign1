81f8f27e2d998da36aa6ac2a87ecdcc45dcc41a5
안동환 <ddh1713flrwls1713@gmail.com>
2024-07-29 22:16:04 +0900

    14assign

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,40 +8,50 @@ public class App { public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
 
-            System.out.println("첫 번째 숫자를 입력하세요: ");
-            // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
-            int num1 = sc.nextInt();
-            System.out.println("두 번째 숫자를 입력하세요: ");
-            // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
-            int num2 = sc.nextInt();
 
-            // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장한다. (charAt(0))
-            System.out.println("사칙 연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
+            while (true) {  // true 를 쓰면 while 문을 조건없이 무한 반복한다는 의미
+                System.out.println("첫 번째 숫자를 입력하세요: ");
+                // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
+                int num1 = sc.nextInt();
+                System.out.println("두 번째 숫자를 입력하세요: ");
+                // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
+                int num2 = sc.nextInt();
 
-            int result = 0;
-            // 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현한다.
+                // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장한다. (charAt(0))
+                System.out.println("사칙 연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
 
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                case '%':
-                    result = num1 % num2;
-                    break;
-                default :
-                    System.out.println("연산이 불가능합니다.");
+                int result = 0;
+                // 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현한다.
+
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                    case '%':
+                        result = num1 % num2;
+                        break;
+                    default :
+                        System.out.println("연산이 불가능합니다.");
+                }
+                System.out.println(result);
+                sc.nextLine();
+
+                System.out.println("더 계산하시겠습니까? (exit 시 입력종료)");
+                String end = sc.nextLine();
+                if (end.equals("exit")) {
+                break;
+                }
             }
-            System.out.println(result);
 
 }
 }
