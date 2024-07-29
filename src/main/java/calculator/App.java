48c0fc67b9d04e5377b504a9250c06ca83812710
안동환 <ddh1713flrwls1713@gmail.com>
2024-07-29 22:03:31 +0900

    12assign

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,7 +15,10 @@ public class App { public static void main(String[] args) {
             // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
             int num2 = sc.nextInt();
 
+            // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장한다. (charAt(0))
+            System.out.println("사칙 연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
 
-    }
+}
 }
