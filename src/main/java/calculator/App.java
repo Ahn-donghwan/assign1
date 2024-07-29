81ee2e4e9994a8816ca6b9d10f756764fe8a8a4e
안동환 <ddh1713flrwls1713@gmail.com>
2024-07-29 21:58:31 +0900

    3rdassign

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,16 +3,10 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class App {
-
-    public static void main(String[] args) {
+public class App { public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String input = "";
 
-        ArrayList<Integer> intList = new ArrayList<>();
-
-        for (; !input.equals("exit"); ) {
 
             System.out.println("첫 번째 숫자를 입력하세요: ");
             // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
@@ -28,66 +22,26 @@ public class App {
             int result = 0;
             // 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현한다.
 
-            if (operator == '+') {
-                result = num1 + num2;
-                System.out.println(" 결과 : " + result);
-            } else if (operator == '-') {
-                result = num1 - num2;
-                System.out.println(" 결과 : " + result);
-            } else if (operator == '*') {
-                result = num1 * num2;
-                System.out.println(" 결과 : " + result);
-            } else if (operator == '/') {
-                if (num2 == 0) {
-                    System.out.println("분모에 0이 들어갈 수 없습니다.");
-                } else {
-                    result = num1 / num2;
-                    System.out.println(" 결과 : " + result);
-                }
-            } else if (operator == '%') {
-                if (num2 == 0) {
-                    System.out.println("분모에 0이 들어갈 수 없습니다.");
-                } else {
-                    result = num1 % num2;
-                    System.out.println(" 결과 : " + result);
-                }
-            } else {
-                System.out.println("다시 입력해주세요.");
-                continue;
-            }
+           switch (operator) {
+               case '+':
+                   result = num1 + num2;
+                   break;
+               case '-':
+                   result = num1 - num2;
+                   break;
+               case '*':
+                   result = num1 * num2;
+                   break;
+               case '/':
+                   result = num1 / num2;
+                   break;
+               case '%':
+                   result = num1 % num2;
+                   break;
+               default :
+                   System.out.println("연산이 불가능합니다.");
+           }
+           System.out.println(result);
 
-            // 결과를 ArrayList에 저장
-            intList.add(result);
-
-            sc.nextLine(); // 버퍼 비우기
-
-            // remove 입력시 삭제
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            input = sc.nextLine();
-            if (input.equals("remove")) {
-                if (!intList.isEmpty()) {
-                    intList.remove(0);
-                    System.out.println("가장 먼저 저장된 값이 삭제됨.");
-                } else {
-                    System.out.println("삭제할 값이 없습니다.");
-                }
-            }
-
-            // inquiry 입력시 조회
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            input = sc.nextLine();
-            if (input.equals("inquiry")) {
-                // for 문 = for ( 변수타입 변수명 : 배열 ) { 연산 }
-                for (int res : intList) {
-                    System.out.println(res);
-                }
-            }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
-            input = sc.nextLine();
-        }
-
-        // 프로그램 종료 시 Scanner 닫기 // 챗지피티가 닫아라고함.
-        sc.close();
     }
 }
