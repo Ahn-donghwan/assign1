eb18e923c03011158e0b7a0ff8b7a90a0377fe3b
안동환 <ddh1713flrwls1713@gmail.com>
2024-07-29 10:22:41 +0900

    1-3complete

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,7 +16,38 @@ public class App {
         int num2 = sc.nextInt();
 
         // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장한다. (charAt(0))
+        System.out.println("사직 연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+
+        int result = 0;
+        // 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현한다.//
 
 
+        if (operator == '+') {
+            result = num1 + num2;
+            System.out.println(" 결과 : " + result);
+        } else if (operator == '-') {
+            result = num1 - num2;
+            System.out.println(" 결과 : " + result);
+        } else if (operator == '*') {
+            result = num1 * num2;
+            System.out.println(" 결과 : " + result);
+        } else if (operator == '/') {
+            if (num2 == 0) {
+                System.out.println("분모에 0이 들어갈 수 없습니다.");
+            }
+            result = num1 / num2;
+            System.out.println(" 결과 : " + result);
+        } else if (operator == '%') {
+            if (num2 == 0) {
+                System.out.println("분모에 0이 들어갈 수 없습니다.");
+            }
+            result = num1 % num2;
+            System.out.println(" 결과 : " + result);
+        } else {
+            System.out.println("다시 입력해주세요.");
+
+
+        }
     }
 }
